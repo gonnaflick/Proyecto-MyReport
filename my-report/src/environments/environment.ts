f4efa3b2ae99750db6229d42f095f22538e4dc38
Jesus Alejandro Jimenez Hernanez <gonnaflick@hotmail.com>
2023-04-21 20:29:20 -0600

    Update environment.ts

--- my-report/src/environments/environment.ts
+++ my-report/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   supabase: {
     publicKey:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkZmxoZXZpbXd4YnFjeGpzZ2VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE1MzIzMDUsImV4cCI6MTk5NzEwODMwNX0.hUWFUXr70UJE4Jr5u1BbAJdIKnl_GpWbTj3ttQMI8MM',
-    url: 'https://fdflhevimwxbqcxjsgem.supabase.co',
+      'YOUR PUBLIC KEY',
+    url: 'YOUR DATABASE LINK',
   },
 };
